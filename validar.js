4b023cc9275fd48555bca26c1336b3b84be13398
ArturoLlamasSoo <llamas.arturo.cb37@gmail.com>
2023-10-26 22:59:27 -0700

    Actualice la contra

--- validar.js
+++ validar.js
@@ -1,7 +1,7 @@
 validar=function() {
     var usuario = document.getElementById('login__username')
     var pass = document.getElementById('login__password')
-    if(usuario.value=="Admin" && pass.value=="1234") {
+    if(usuario.value=="Admin" && pass.value=="6121824") {
         location.href ="CASA.html";
         }
          else {
